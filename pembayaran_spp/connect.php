8e11422954d1bebaae393180c94127e4e1f305d5
KenzieL16 <105412597+KenzieL16@users.noreply.github.com>
2022-10-31 10:43:49 +0700

    Delete pembayaran_spp directory

--- pembayaran_spp/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$connect=mysqli_connect('localhost','root','','pembayaran_spp');
-/* check connection */
-if (mysqli_connect_error()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
-}
-
-?>
--- pembayaran_spp/home.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    include "header.php";
-?>
-<h2>Selamat datang <?=$_SESSION['nama']?> di website Pembayaran SPP.</h2>
-<!-- <?php
-    include "footer.php";
-?> -->
